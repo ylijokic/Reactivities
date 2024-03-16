303cc10451a2e1e089e5d8b3fd668c829f808aad
ylijokic <ylijokic@gmail.com>
2024-03-16 12:08:44 -0700

    Update API Controllers

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -12,9 +12,15 @@ namespace API.Controllers
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete( string id)
+        public async Task<IActionResult> Delete(string id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMain( string id)
+        {
+            return HandleResult(await Mediator.Send(new SetMain.Command{Id = id}));
         }
     }
 }
