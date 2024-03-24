a7e63dcc9ede8827ea455d4b165cbf095f61f2de
ylijokic <ylijokic@gmail.com>
2024-03-24 11:09:31 -0700

    Create User Profile Page

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -14,13 +14,13 @@ export default function ActivityListItem({ activity }: Props) {
                 {activity.isCancelled && <Label attached='top' color='red' content='Cancelled' style={{textAlign: 'center'}} />}
                 <Item.Group>
                     <Item>
-                        <Item.Image style={{marginBottom: 3}} size='tiny' circular src='/assets/user.png'/>
+                        <Item.Image style={{marginBottom: 3}} size='tiny' circular src={activity.host?.image ?? '/assets/user.png'}/>
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
                             <Item.Description>
-                                Hosted By {activity.host?.displayName}
+                                Hosted By <Link to={`/profiles/${activity.host?.username}`}> {activity.host?.displayName}</Link>
                             </Item.Description>
                             {activity.isHost && (
                                 <Item.Description>
